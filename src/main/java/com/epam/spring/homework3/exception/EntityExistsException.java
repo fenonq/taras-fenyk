64d513f012ba45c08a1f7b0f4444f83a0b765600
fenonq <fenyk.ttt@gmail.com>
2022-08-24 18:58:51 +0300

    added missing errorType

--- src/main/java/com/epam/spring/homework3/exception/EntityExistsException.java
+++ src/main/java/com/epam/spring/homework3/exception/EntityExistsException.java
@@ -1,5 +1,7 @@
 package com.epam.spring.homework3.exception;
 
+import com.epam.spring.homework3.model.enums.ErrorType;
+
 public class EntityExistsException extends ServiceException {
 
     private static final String DEFAULT_MESSAGE = "Entity already exists!";
@@ -10,6 +12,11 @@ public class EntityExistsException extends ServiceException {
 
     public EntityExistsException(String message) {
         super(message);
+    }
+
+    @Override
+    public ErrorType getErrorType() {
+        return ErrorType.VALIDATION_ERROR_TYPE;
     }
 
 }
